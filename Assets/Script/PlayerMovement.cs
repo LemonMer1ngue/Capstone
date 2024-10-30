ceb9171fce4739c1ea820ce6568061220e25217a
fathrdm <114979588+fathrdm@users.noreply.github.com>
2024-10-30 18:14:10 +0800

    Fathiah/Update Playermovement

--- Assets/Script/PlayerMovement.cs
+++ Assets/Script/PlayerMovement.cs
@@ -99,27 +99,6 @@ public class PlayerMovement : MonoBehaviour
                     isHoldingBox = false; break;
 
             }
-
-            //    if (!isHoldingBox)
-            //    {
-
-            //        Box = hit.collider.gameObject;
-            //        Box.GetComponent<FixedJoint2D>().enabled = true;
-            //        Box.GetComponent<InteractBox>().beingPushed = true;
-            //        Box.GetComponent<FixedJoint2D>().connectedBody = this.GetComponent<Rigidbody2D>();
-            //        isHoldingBox = true;
-
-            //}
-            //else
-            //    {
-            //        Box.GetComponent<FixedJoint2D>().enabled = false;
-            //    Box.GetComponent<InteractBox>().beingPushed = false;
-            //    Box.GetComponent<FixedJoint2D>().connectedBody = null;
-            //        Box = null; 
-            //        isHoldingBox = false;
-
-            //}
-
         }
     }
     void UpdateInteractText()
